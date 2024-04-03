695d5dd9b4ab6a67da7c15daa4e896e9568333a9
TelurXS <vacaroslav@gmail.com>
2024-04-03 15:50:37 +0300

    LR5 and LR6

---
+++ LR5/LR5/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LR5
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void OnTextChanged(object sender,TextChangedEventArgs e)
+		{
+			try
+			{
+				char c0 = char.Parse(TextBox_C0.Text);
+				char c1 = char.Parse(TextBox_C1.Text);
+				char c2 = char.Parse(TextBox_C2.Text);
+
+				int value = int.Parse($"{c0}{c1}{c2}");
+
+				Label_Result.Content = value.ToString();
+			}
+			catch (Exception ex)
+			{
+				Label_Result.Content = ex.Message;
+			}
+		}
+	}
+}
